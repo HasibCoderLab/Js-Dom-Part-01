ccfd21795d5a89c193e3c36ad6ba328b7549cb5c
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-19 15:30:52 +0600

    learn  how to chnage with ClassName

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log(document.body);
-console.dir(document.body.childNodes[1].innerText = "Hi")
+// console.log(document.body);
+// // console.dir(document.body.childNodes[1].innerText = "Hi")
+let  heading = document.getElementById("head") ;
+heading.innerText = "AstroMachin Team NASA" ;
+// Using ClassName
+let extra = document.getElementsByClassName("extra");
+extra[0].innerText  = "Dhaka";
+ extra[1].innerText  = "Team-Leader : Muen (Arduino Expart & Robotics Engg)";
